7e7d96679ce5c6861aa9bf20278aa3251915fb05
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-09-23 10:02:52 -0700

    added more tests and zoneid to config

--- utils.py
+++ utils.py
@@ -4,29 +4,46 @@ import typer
 app = typer.Typer(no_args_is_help=True, add_completion=False)
 
 
-def load_config():
-    with open('.config.yaml', 'r') as f:
-        myyaml = safe_load(f)
-    return myyaml
+class awsClass:
+    def __init__(self):
+        self.config = self.load_config()
+        self.get_hosted_zone()
+        self.connect()
 
+    def load_config(self):
+        with open('.config.yaml', 'r') as f:
+            myyaml = safe_load(f)
+        return myyaml
 
-def connect():
-    conf = load_config()
-    conn = route53.connect(
-        aws_access_key_id=conf['ACCESS_KEY'],
-        aws_secret_access_key=conf['SECRET_KEY'],
-    )
-    return conn
+    def connect(self):
+        conn = route53.connect(
+            aws_access_key_id=self.config['ACCESS_KEY'],
+            aws_secret_access_key=self.config['SECRET_KEY'],
+        )
+        self.conn = conn
+        return conn
+
+    def get_hosted_zone(self):
+        self.zone = self.conn.get_hosted_zone_by_id(self.config['ZONEID'])
+        return self.zone
 
 
 @app.command('configtest')
 def config_test():
-    print(load_config())
+    testing = awsClass()
+    print(testing.config)
 
 
 @app.command('connecttest')
 def connection_test():
-    print(connect())
+    testing = awsClass()
+    print(testing.conn)
+
+
+@app.command('hostedTest')
+def hosted_zone_test():
+    testing = awsClass()
+    print(testing.zone)
 
 
 if __name__ == "__main__":
